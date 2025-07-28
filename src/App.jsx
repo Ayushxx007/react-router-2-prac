2c77f3df3ae67214dcc28347c56b8d052ca0bbd8
Ayush Kumawat <ayushkumawat77889@gmail.com>
2025-07-28 13:38:44 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router'
 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline">
       mahika
+     
+      <h1>Welcome to the App</h1>
+
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Routes ,Route} from 'react-router'
+import About from './Components/About.jsx'
+import Contact from './Components/Contact.jsx'
+import Body from './Components/Body.jsx'
+import {BrowserRouter} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+
+        <Route path="" element={<Body />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      
+      </Route>
+
+      </Routes>
+   
+
     <App />
+       </BrowserRouter>
   </StrictMode>,
+
 )
